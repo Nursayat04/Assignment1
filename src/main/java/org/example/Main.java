606b203768f35cf6801334169814ed4c649a5386
Nurca <galnur2022@gmail.com>
2025-02-16 01:16:46 +0500

    last commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
-
+        Order order = context.getBean(Order.class);
+        System.out.println(order.getName());
+        System.out.println(order.getPrice());
+        Barista barista = context.getBean(Barista.class);
+        System.out.println(barista.getBaristaName());
+        System.out.println(barista.getBaristaAge());
     }
 }
